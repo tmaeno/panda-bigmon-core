ff2cd728f03655a864e1c4305547c9b41b26314b
SergeyPod <spodolsky@mail.ru>
2016-04-11 18:15:04 -0400

    added cache to taskESExtendendInfo

--- core/views.py
+++ core/views.py
@@ -3984,7 +3984,7 @@ def dashTasks(request, hours, view='production'):
         }
         return  HttpResponse(json.dumps(data), mimetype='text/html')
 
-
+@cache_page(60*6)
 def taskESExtendedInfo(request):
 
     if 'jeditaskid' in request.REQUEST:
