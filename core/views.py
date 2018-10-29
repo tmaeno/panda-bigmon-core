a2cd61e0baa90c8b83c6e0ee1d1db50082d0ee89
tkorchug <tatiana.korchuganova@cern.ch>
2018-10-29 15:30:33 +0100

    forcing of taskInfo cache refreshing turned off

--- core/views.py
+++ core/views.py
@@ -8036,7 +8036,7 @@ def taskInfo(request, jeditaskid=0):
                         doRefresh = True
                 else:
                     doRefresh = True
-        doRefresh = True
+        # doRefresh = True
 
         ### This is a temporary fix in order of avoiding 500 error for cached tasks not compartible to a new template
         if not isinstance(data['jobscoutids']['ramcountscoutjob'], list):
@@ -8584,7 +8584,7 @@ def taskInfoNew(request, jeditaskid=0):
                 else:
                     doRefresh = True
         # temp turning on refresh of all tasks to rewrite cache
-        doRefresh = True
+        # doRefresh = True
 
         ### This is a temporary fix in order of avoiding 500 error for cached tasks not compartible to a new template
         if not isinstance(data['jobscoutids']['ramcountscoutjob'], list):
